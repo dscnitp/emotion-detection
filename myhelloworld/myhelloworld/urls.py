4bc1179dc33e3a64004f662318f021b03fcc2019
Aparna Juhi <54853999+AparnaJuhi@users.noreply.github.com>
2021-02-04 19:45:30 +0530

    Updated

--- myhelloworld/myhelloworld/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home),
-    path('count/',views.count,name='count'), 
-    path('home/', views.home,name='home')
-]
